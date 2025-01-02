3580cf089908228ba8dfaf18023f5cba11b4ea56
NGamerHere <codingdatta@gmail.com>
2025-01-02 17:36:34 +0530

    added the prisma and all

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,18 @@
+import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    return NextResponse.json({ message: 'Hello from Next.js!' });
+    const users = await prisma.AppUser.findMany();
+    return NextResponse.json(users);
+}
+
+export async function POST(req: Request) {
+    const body = await req.json();
+    const { name, email } = body;
+
+    const user = await prisma.AppUser.create({
+        data: { name, email },
+    });
+
+    return NextResponse.json(user);
 }
